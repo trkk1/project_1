a8e0fbf305545c6f7af6e6121792f05657797963
trkk1 <trankiett0911@gmail.com>
2024-09-03 00:55:29 -0700

    Initial Commit

---
+++ CIS25/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  CIS25
+//
+//  Created by Minh Tran on 9/3/24.
+//
+
+#include <iostream>
+//using namespace std;
+
+int main(){
+
+    int num1 = 2;
+    int num2 = 1;
+    std :: cout << "Hello\n" << num1 << "\n" << num2 << "\n";
+    std :: swap(num1, num2);
+    std :: cout << "After swapping\n" << num1 << "\n" << num2 << "\n";
+    return 0;
+}
