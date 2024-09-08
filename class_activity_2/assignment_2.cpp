69fb3eb8df0bf46a149f9a8879d5b04e67beb092
trkk1 <trankiett0911@gmail.com>
2024-09-08 13:34:40 -0700

    new added

--- class_activity_2/assignment_2.cpp
+++ class_activity_2/assignment_2.cpp
@@ -6,3 +6,20 @@
 //
 
 #include "assignment_2.hpp"
+
+#include <iostream>
+
+int main(){
+    int made = 95000;
+    double estateTax = 0.04;
+    double countyTax = 0.02;
+    
+    double totalEstateTax = made * estateTax;
+    double totalCountyTax = made * countyTax;
+    double totalTax = totalEstateTax + totalCountyTax;
+    
+    std :: cout << "Total Sales: $" << made << std :: endl;
+    std :: cout << "Total Estate Tax: $" << totalEstateTax << std :: endl;
+    std :: cout << "Total County Tax: $" << totalCountyTax << std :: endl;
+    std :: cout << "Total Tax: $" << totalTax << std :: endl;
+}
