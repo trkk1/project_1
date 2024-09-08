f1cd90d03c5f0dfa2368b6c984833061291b5ac5
trkk1 <trankiett0911@gmail.com>
2024-09-08 13:17:06 -0700

    new updated

---
+++ class_activity_1/assignment_1.cpp
@@ -0,0 +1,8 @@
+//
+//  assignment_1.cpp
+//  CIS25
+//
+//  Created by Minh Tran on 9/8/24.
+//
+
+#include "assignment_1.hpp"
--- class_activity_1/assignment_1.hpp
+++ class_activity_1/assignment_1.hpp
@@ -2,7 +2,7 @@
 //  assignment_1.hpp
 //  CIS25
 //
-//  Created by Minh Tran on 9/4/24.
+//  Created by Minh Tran on 9/8/24.
 //
 
 #ifndef assignment_1_hpp
