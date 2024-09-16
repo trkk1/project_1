e9a0519348f850c72cd632b982404cb518186114
trkk1 <trankiett0911@gmail.com>
2024-09-15 22:09:29 -0700

    new added fixed so the result is double value ( cis25 )

--- class_activity_8/assignment_8.cpp
+++ class_activity_8/assignment_8.cpp
@@ -6,3 +6,18 @@
 //
 
 #include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main (){
+    int x = 10;
+    int y = 3;
+    
+    //Cast to double
+    double z = static_cast<double>(x) / y;
+
+    cout << z << endl;
+
+    return 0;
+
+}
