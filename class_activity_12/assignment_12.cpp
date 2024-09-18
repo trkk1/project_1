ee092d666cb7b32f21d17a7754d28e70ae927a5a
trkk1 <trankiett0911@gmail.com>
2024-09-17 22:05:38 -0700

    new added

--- class_activity_12/assignment_12.cpp
+++ class_activity_12/assignment_12.cpp
@@ -6,3 +6,22 @@
 //
 
 #include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main(){
+    int positiveNumber;
+    int fact = 1;
+    
+    cout << "Enter a postive number" << endl;
+    cin >> positiveNumber;
+
+    
+    for (int i = 1; i <= positiveNumber; i++)
+        fact = fact * i;
+
+    cout << "The factorial of the positive number is: " << fact << endl;
+
+    return 0;
+    
+}
--- work_space/lecture_sept_16.cpp
+++ work_space/lecture_sept_16.cpp
@@ -6,3 +6,83 @@
 //
 
 #include "lecture_sept_16.hpp"
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+    
+    //for (int i = 0; i <= 5; i++)
+        //cout << i << endl;
+    
+    long random = time(0);
+    srand(random);
+    int secretNumber = rand() % 10;
+    int guessNum = 0;
+    bool check = true;
+    
+    cout << secretNumber << endl;
+    
+    while (check){
+        cout << "guess the secret number" << endl;
+        cin >> guessNum;
+        if (guessNum == secretNumber){
+            cout << "you guessed the correct secret number" << endl;
+            check = false;
+        }
+    }
+    
+    return 0;
+}
+
+/* //While loop
+ 
+ int i = 0;
+ while (i <= 5){
+     cout << i << endl;
+     i++;
+ }
+ 
+ */
+
+/* //For loop
+ 1) For numbers
+ int numbers[] = {1, 2, 3, 4};
+ 
+ 
+ for (int i = 1; i < sizeof(numbers)/sizeof(int); i++)
+     cout << numbers[i] << endl;
+ 
+ for (int number:numbers)
+     cout << number << endl;
+ 
+ 2) For characters
+ string name = "Minh";
+ 
+ for (char ch:name){
+     cout << ch << endl;
+ }
+
+ 
+ */
+
+
+/* //Ask a postive number then print the factorial of that positive number
+ 
+ int positiveNumber;
+ int fact = 1;
+ 
+ cout << "Enter a postive number" << endl;
+ cin >> positiveNumber;
+
+ 
+ for (int i = 1; i <= positiveNumber; i++)
+     fact = fact * i;
+
+ cout << "The factorial of the positive number is: " << fact << endl;
+
+ return 0;
+ 
+ */
