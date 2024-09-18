48bb9740e556efcd538eb1f955734570a6de9160
trkk1 <trankiett0911@gmail.com>
2024-09-17 22:17:48 -0700

    guess the secret number

---
+++ class_activity_13/assignment_13.cpp
@@ -0,0 +1,37 @@
+//
+//  assignment_13.cpp
+//  CIS25
+//
+//  Created by Minh Tran on 9/17/24.
+//
+
+#include <stdio.h>
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main(){
+    
+    long random = time(0);
+    srand(static_cast<int>(random));
+    int secretNumber = rand() % 10;
+    int guessNum = 0;
+    bool check = true;
+    
+    cout << "guess the secret number" << endl;
+
+    while (check){
+        cin >> guessNum;
+        if (guessNum == secretNumber){
+            cout << "you guessed the correct secret number" << endl;
+            check = false;
+        }
+        else{
+            cout << "not correct. guess the secret number again" << endl;
+        }
+    }
+    
+    return 0;
+}
