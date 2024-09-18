1ab042cc67d0ceca4d99ba515c7a03905695fad4
trkk1 <trankiett0911@gmail.com>
2024-09-17 23:03:35 -0700

    asked for temp and return average temp

--- class_activity_13/assignment_13.cpp
+++ class_activity_13/assignment_13.cpp
@@ -13,25 +13,50 @@
 using namespace std;
 
 int main(){
+    double sum = 0.0;
+    double temp = 0.0;
+    int count = 0;
+    string option;
     
-    long random = time(0);
-    srand(static_cast<int>(random));
-    int secretNumber = rand() % 10;
-    int guessNum = 0;
-    bool check = true;
     
-    cout << "guess the secret number" << endl;
-
-    while (check){
-        cin >> guessNum;
-        if (guessNum == secretNumber){
-            cout << "you guessed the correct secret number" << endl;
-            check = false;
+    cout << "Enter a temperature: ";
+    cin >> temp;
+    
+    //initialize the sum and count with the first temperature
+    sum += temp;
+    count++;
+    
+    cout << "Do you want to enter another temperature? (yes or no)" << endl;
+    cin >> option;
+    
+    while (true) {
+        if (option == "no" || option == "No"){
+            break; //exit the loop or terminate the loop
+        }
+        else if (option == "yes" || option == "Yes"){
+            double temperature;
+            cout << "Enter another temperature: ";
+            cin >> temperature;
+            
+            //add new temperature to the sum and increment the count.
+            sum += temperature;
+            count++;
+            
+            cout << "Do you want to enter another temperature? (yes or no)" << endl;
+            cin >> option;
         }
         else{
-            cout << "not correct. guess the secret number again" << endl;
+            cout << "Invalid option. Please enter 'yes' or 'no'!" << endl;
+            cin >> option;
         }
     }
     
+        if (count > 0){
+            double averageTemp = sum / count;
+            cout << "The average temperature is: " << averageTemp << endl;
+        }
+        else {
+            cout << "No temperatures were entered." << endl;
+        }
     return 0;
 }
